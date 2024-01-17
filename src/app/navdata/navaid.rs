7cfaef4678833422b5a53b7656ebfc94310b8813
Leirn <laurent@vromman.org>
2024-01-17 12:38:34 +0100

    Use new model module

--- src/app/navdata/navaid.rs
+++ src/app/navdata/navaid.rs
@@ -1,11 +1,11 @@
+use crate::app::{
+    db::AppState,
+    navdata::model::{get_navaid_by_icao_code, search_navaid},
+};
 use actix_web::{get, web, HttpResponse, Responder};
 use log::{error, info};
 use serde::Deserialize;
-use serde_json::{json, Value};
-use sqlite::State;
-use std::error::Error;
-
-use crate::app::{db::AppState, messages::ERROR_SQLITE_ACCESS};
+use serde_json::json;
 
 pub fn register_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(navaid);
@@ -20,47 +20,6 @@ struct FormData {
     search: Option<String>,
 }
 
-async fn search_navaid(
-    search: Option<String>,
-    _page: Option<u32>,
-    app_state: web::Data<AppState>,
-) -> Result<Value, Box<dyn Error>> {
-    let con = app_state
-        .sqlite_connection
-        .lock()
-        .expect(ERROR_SQLITE_ACCESS);
-
-    let mut statement = match search {
-        Some(search) => {
-            let query = "SELECT * FROM navaids WHERE icao_code LIKE '%' || ? || '%' OR name LIKE '%' || ? || '%' OR associated_airport LIKE '%' || ? || '%' LIMIT 100";
-            let mut s = con.prepare(query)?;
-            s.bind((1, search.as_str()))?;
-            s.bind((2, search.as_str()))?;
-            s.bind((3, search.as_str()))?;
-            s
-        }
-        None => {
-            let query = "SELECT * FROM navaids LIMIT 100";
-            con.prepare(query)?
-        }
-    };
-
-    let mut data = json!([]);
-
-    while let Ok(State::Row) = statement.next() {
-        let mut navaid_data = json!({});
-
-        for column_name in statement.column_names() {
-            navaid_data.as_object_mut().unwrap().insert(
-                column_name.clone(),
-                json!(statement.read::<String, _>(column_name.as_str())?),
-            );
-        }
-        data.as_array_mut().unwrap().push(navaid_data);
-    }
-    Ok(data)
-}
-
 #[get("/navaid")]
 async fn navaid(param: web::Query<FormData>, app_state: web::Data<AppState>) -> impl Responder {
     info!("Request received : /navaid");
@@ -72,37 +31,6 @@ async fn navaid(param: web::Query<FormData>, app_state: web::Data<AppState>) -> 
             HttpResponse::Ok().json(json!({"status": "error"}))
         }
     }
-}
-
-async fn get_navaid_by_icao_code(
-    icao: String,
-    app_state: web::Data<AppState>,
-) -> Result<Value, Box<dyn Error>> {
-    let query = "SELECT * FROM navaids WHERE icao_code=?";
-
-    let icao = icao.to_uppercase();
-
-    let mut data = json!([]);
-
-    let con = app_state
-        .sqlite_connection
-        .lock()
-        .expect(ERROR_SQLITE_ACCESS);
-    let mut statement = con.prepare(query)?;
-    statement.bind((1, icao.as_str()))?;
-
-    while let Ok(State::Row) = statement.next() {
-        let mut navaid_data = json!({});
-
-        for column_name in statement.column_names() {
-            navaid_data.as_object_mut().unwrap().insert(
-                column_name.clone(),
-                json!(statement.read::<String, _>(column_name.as_str())?),
-            );
-        }
-        data.as_array_mut().unwrap().push(navaid_data);
-    }
-    Ok(data)
 }
 
 #[get("/navaid/{icao}")]
